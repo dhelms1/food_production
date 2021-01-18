f8f7cf43e037365fff40b7f4f2552aefda0df95b
dhelms1 <48806608+dhelms1@users.noreply.github.com>
2021-01-18 10:06:33 -0800

    python code optimized, graphs changed to better visual, more details added

--- functions.py
+++ functions.py
@@ -43,52 +43,79 @@ def plot_top_20_countries(data):
     '''
     Create a barplot of the top 20 producing countries over the last 50 years. 
     '''
-    prod_by_country = {}
-    for country in data.Area.unique(): # create dictionary with country and total production (all food items)
-        prod_by_country[country] = data[data.Area == country]['TotalProd'].values.sum()
-    
-    prod_df = pd.DataFrame(list(prod_by_country.items()), columns=['Country', 'Production'])
-    sorted_prod = prod_df.sort_values(by='Production', ascending=False).iloc[:20, :] # top 20 rows
+    # Get top 20 producing countries    
+    prod_df = pd.DataFrame(data.groupby('Area')['TotalProd'].agg('sum').sort_values(ascending=False))[:20]
     
     plt.figure(figsize=(12,8))
-    sns.barplot(x='Production', y='Country', data=sorted_prod)
+    sns.barplot(x=prod_df.TotalProd, y=prod_df.index)
     plt.title('Top 20 Producing Countries')
     plt.xlabel('Production (in 1000 tonnes)')
     plt.show();
     
-    prod_df = None # clear temporary dataframe
-    sorted_prod = None # clear temporary dataframe
+    prod_df = None # clear temporary dataframes
     return None
 
 
 
 def plot_yearly_prod(data):
     '''
-    Plot the top 3 countries production trend over the last 50 years. The country
-    names are hard coded into the function and will need to be changed if needed.
+    Plot the top 10 countries production trends over the last 50 years.
     '''
-    top_3 = ['China, mainland', 'United States of America', 'India']
+    top_10 = data.groupby('Area')['TotalProd'].agg('sum').sort_values(ascending=False).index[:10].values
 
-    top_3_df = pd.DataFrame()
-    top_3_df['Index'] = data.columns[5:-1].values
+    top_10_df = pd.DataFrame()
+    top_10_df['Index'] = data.columns[5:-1].values
 
-    for country in top_3:
-        top_3_df[country] = data[data.Area == country].iloc[:, 5:-1].sum(axis=0).values
+    for country in top_10:
+        top_10_df[country] = data[data.Area == country].iloc[:, 5:-1].sum(axis=0).values
 
-    top_3_df.set_index('Index', inplace=True)
-    top_3_df = top_3_df.T  
+    top_10_df.set_index('Index', inplace=True)
+    top_10_df = top_10_df.T  
     
-    colors = ['red', 'orange', 'green']
     plt.figure(figsize=(16,8))
     x = np.arange(1,54)
-    for i in range(3):
-        sns.lineplot(x=top_3_df.columns.values, y=top_3_df.values[i], color=colors[i])
-    plt.legend(top_3_df.index.values)
+    for i in range(10):
+        sns.lineplot(x=top_10_df.columns.values, y=top_10_df.values[i], color=sns.color_palette("hls", 10)[i])
+    plt.legend(top_10_df.index.values)
     plt.xticks(rotation='vertical')
-    plt.title('Top 3 Producing Countries from 1961 to 2013')
+    plt.title('Top 10 Producing Countries from 1961 to 2013')
     plt.ylabel('Production (in 1000 tonnes)')
     plt.show();
     
+    top_10_df = None # clear temporary dataframe
+    
+    return None
+
+
+
+def plot_top_20_food(data):
+    '''
+    Create a barplot of the top 20 produced items over the last 50 years.
+    '''
+    # Get top 20 products by quantity produced    
+    prod_df = pd.DataFrame(data.groupby('Item')['TotalProd'].agg('sum').sort_values(ascending=False))[:20]
+    
+    # Get top 20 products by counts
+    count_df = pd.DataFrame(data.groupby("Item")["Element"].agg("count").sort_values(ascending=False))[:20]
+    
+    # Plot both figures
+    plt.figure(figsize=(12,10))
+    ax1 = plt.subplot(2,1,1)
+    sns.barplot(x=prod_df.TotalProd, y=prod_df.index)
+    ax1.title.set_text('Top 20 Produced Items (by total quantity)')
+    plt.xlabel('Production (in 1000 tonnes)')
+    plt.ylabel(' ')
+    
+    ax2 = plt.subplot(2,1,2)
+    sns.barplot(x=count_df.Element, y=count_df.index)
+    ax2.title.set_text('Top 20 Produced Items (by occurance)')
+    plt.xlabel('Occurance in Data')
+    plt.ylabel(' ')
+    
+    plt.tight_layout()
+    plt.show();
+    
+    prod_df = count_df = None # clear temporary dataframes
     return None
 
 
@@ -98,41 +125,30 @@ def top_20_feed_food(feed, food):
     Create two plots, showing the top 20 produced food items for both human
     and livestock consumption.
     '''
-    top_feed_prod = {}
-    top_food_prod = {}
-
-    for item in feed.Item.unique(): # Iterate through feed items
-        top_feed_prod[item] = feed[feed.Item == item]['TotalProd'].sum()
-
-    for item in food.Item.unique(): # Iterate through food items
-        top_food_prod[item] = food[food.Item == item]['TotalProd'].sum()
-
     # Create feed dataframe, sort by descending, and select the top 20 rows
-    feed_df = pd.DataFrame(list(top_feed_prod.items()), columns=['Item', 'Production'])
-    feed_sorted = feed_df.sort_values(by='Production', ascending=False).iloc[:20, :] # top 20 rows
+    feed_df = pd.DataFrame(feed.groupby('Item')['TotalProd'].agg('sum').sort_values(ascending=False))[:20]
 
     # Create food dataframe, sort by descending, and select the top 20 rows
-    food_df = pd.DataFrame(list(top_food_prod.items()), columns=['Item', 'Production'])
-    food_sorted = food_df.sort_values(by='Production', ascending=False).iloc[:20, :] # top 20 rows
+    food_df = pd.DataFrame(food.groupby('Item')['TotalProd'].agg('sum').sort_values(ascending=False))[:20]
 
     # Plot both figures as subplots to compare
-    plt.figure(figsize=(15,6))
-    ax1 = plt.subplot(1,2,1)
+    plt.figure(figsize=(12,10))
+    ax1 = plt.subplot(2,1,1)
     ax1.title.set_text('Top 20 Feed (Livestock) Products')
-    sns.barplot(x='Production', y='Item', data=feed_sorted)
+    sns.barplot(x=feed_df.TotalProd, y=feed_df.index)
     plt.xlabel('Production (in 1000 tonnes)')
     plt.ylabel(' ') # remove y label
 
-    ax2 = plt.subplot(1,2,2)
+    ax2 = plt.subplot(2,1,2)
     ax2.title.set_text('Top 20 Food (Human) Products')
-    sns.barplot(x='Production', y='Item', data=food_sorted)
+    sns.barplot(x=food_df.TotalProd, y=food_df.index)
     plt.xlabel('Production (in 1000 tonnes)')
     plt.ylabel(' ') # remove y label
 
     plt.tight_layout()
 
     # Clear dataframes for memory purposes (not sure if necessary but good for practice)
-    feed_df = food_df = feed_sorted = food_sorted = None
+    feed_df = food_df = None
     
     return None
 
